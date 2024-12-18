d1156b6f982bc2f54dce85f5846edfd4b71b77f5
Abdullahi Aminu Mudi <68612062+Abdulfortech@users.noreply.github.com>
2024-12-18 22:47:12 +0100

    Update apiRequest.js

--- src/lib/apiRequest.js
+++ src/lib/apiRequest.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 
 const apiRequest= axios.create({
-    baseURL:"http://api.mufaldatasub.com",
+    baseURL:"http://api.mufaldatasub.com/api",
     // baseURL:"http://localhost:8800/api",
     withCredentials:true,
 })
