2251418804feb2bb4a3c77a299f2bccc6631423e
abdulfortech <ibnmudi@gmail.com>
2024-12-25 17:19:10 +0100

    changes

--- src/lib/apiRequest.js
+++ src/lib/apiRequest.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 
 const apiRequest= axios.create({
-    baseURL:"http://api.mufaldatasub.com/api",
+    baseURL:"https://api.mufaldatasub.com/api",
     // baseURL:"http://localhost:8800/api",
     withCredentials:true,
 })
